312a5b474e2689036653adcc659da83e04be9813
Shakh7 <ssharipov3@students.wiut.uz>
2024-06-08 18:41:22 +0500

    home page

--- shakha/src/app/page.tsx
+++ shakha/src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
         {name: "Home", href: "/"},
     ];
 
-
     return (
         <div
             className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
